014331e9fdf0e467c7249c4191649f27fc4198ca
Delivator <info@delivator.me>
2020-08-15 17:44:38 +0200

    Added ace code editor

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="12">
+        <div id="editor"></div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  import { ace } from "../assets/ace";
+
+  export default {
+    name: 'Home',
+
+    beforeCreate: function() {
+      const editor = ace.edit("editor");
+      editor.setTheme("ace/theme/monokai");
+      editor.session.setMode("ace/mode/javascript");
+    },
+  }
+</script>
