cf9c8a9aaa016472a3e95c815104b711601d6a71
Delivator <info@delivator.me>
2020-08-17 21:48:17 +0200

    Updated styling

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,9 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <v-toolbar-title class="headline text-uppercase" to="/">
+        Skybox
+      </v-toolbar-title>
 
       <v-spacer></v-spacer>
 
@@ -47,10 +31,6 @@ export default {
 
   components: {
     Home
-  },
-
-  data: () => ({
-    //
-  })
+  }
 };
 </script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,50 +1,60 @@
 <template>
-  <v-container fluid>
-    <v-row>
-      <v-col cols="4">
+  <v-container fluid class="main-container">
+    <v-row no-gutters>
+      <v-col lg="4" md="12">
+        <codemirror :value="codePresets[0].html" :options="cmOptions" />
+      </v-col>
+      <v-col lg="4" md="12">
         <codemirror
-          ref="cmEditor"
-          :value="codePresets[0].html"
-          :options="cmOptionsHTML"
-          @ready="onCmReady"
-          @focus="onCmFocus"
-          @input="onCmCodeChange"
+          class="editorBorder"
+          :value="codePresets[0].js"
+          :options="{ ...cmOptions, mode: 'text/javascript' }"
         />
       </v-col>
-      <v-col cols="4">
-        <codemirror :value="codePresets[0].js" :options="cmOptionsJS" />
+      <v-col lg="4" md="12">
+        <codemirror
+          :value="codePresets[0].css"
+          :options="{ ...cmOptions, mode: 'text/css' }"
+        />
       </v-col>
-      <v-col cols="4">
-        <codemirror :value="codePresets[0].css" :options="cmOptionsCSS" />
+      <v-col cols="12">
+        <iframe :src="outputUrl" frameborder="0"></iframe>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
-<style scoped>
+<style>
 .CodeMirror {
-  height: auto;
+  height: 50vh;
+}
+
+.editorBorder {
+  border-right: solid grey 1px;
+  border-left: solid grey 1px;
+}
+
+.main-container {
+  padding: 0;
 }
 </style>
 
 <script>
-// import { ace } from "../assets/ace";
+// codemirror imports
 import { codemirror } from "vue-codemirror";
-
 // import base style
 import "codemirror/lib/codemirror.css";
-
 // import language js
 import "codemirror/mode/javascript/javascript.js";
 import "codemirror/mode/htmlmixed/htmlmixed.js";
 import "codemirror/mode/css/css.js";
-
 // import theme style
 import "codemirror/theme/base16-dark.css";
 
-import defaultHTML from "raw-loader!../../templates/default/index.html";
-import defaultJS from "raw-loader!../../templates/default/main.js";
+// import editor templates
+import defaultHTML from "!raw-loader!../../templates/default/index.html";
 import defaultCSS from "!raw-loader!../../templates/default/style.css";
+import defaultJS from "!raw-loader!../../templates/default/main.js";
 
 export default {
   name: "Home",
@@ -53,23 +63,10 @@ export default {
 
   data() {
     return {
-      cmOptionsHTML: {
+      outputUrl: "",
+      cmOptions: {
         tabSize: 4,
         mode: "text/html",
-        theme: "base16-dark",
-        lineNumbers: true,
-        line: true
-      },
-      cmOptionsJS: {
-        tabSize: 4,
-        mode: "text/javascript",
-        theme: "base16-dark",
-        lineNumbers: true,
-        line: true
-      },
-      cmOptionsCSS: {
-        tabSize: 4,
-        mode: "text/css",
         theme: "base16-dark",
         lineNumbers: true,
         line: true
@@ -87,13 +84,6 @@ export default {
   methods: {
     onCmReady(cm) {
       console.log("the editor is readied!", cm);
-    },
-    onCmFocus(cm) {
-      console.log("the editor is focused!", cm);
-    },
-    onCmCodeChange(newCode) {
-      console.log("this is new code", newCode);
-      this.code = newCode;
     }
   }
 };
