c4a99f6d4010b42bbdf50dd75f7666b6e388ae94
Delivator <info@delivator.me>
2020-08-17 14:51:03 +0200

    Added vue codemirror

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,7 @@
   <v-container fluid>
     <v-row>
       <v-col cols="12">
-        <div id="editor"></div>
+        <codemirror v-model="code" :options="cmOptions" />
       </v-col>
     </v-row>
   </v-container>
@@ -10,14 +10,46 @@
 
 <script>
   // import { ace } from "../assets/ace";
+  import {  codemirror } from "vue-codemirror"
 
   export default {
     name: 'Home',
 
-    // beforeCreate: function() {
-    //   const editor = ace.edit("editor");
-    //   editor.setTheme("ace/theme/monokai");
-    //   editor.session.setMode("ace/mode/javascript");
-    // },
+    components: { codemirror },
+
+      data () {
+        return {
+          code: 'const a = 10',
+          cmOptions: {
+            tabSize: 4,
+            mode: 'text/javascript',
+            theme: 'base16-dark',
+            lineNumbers: true,
+            line: true,
+            // more CodeMirror options...
+          }
+        }
+      },
+      methods: {
+        onCmReady(cm) {
+          console.log('the editor is readied!', cm)
+        },
+        onCmFocus(cm) {
+          console.log('the editor is focused!', cm)
+        },
+        onCmCodeChange(newCode) {
+          console.log('this is new code', newCode)
+          this.code = newCode
+        }
+      },
+      computed: {
+        codemirror() {
+          return this.$refs.cmEditor.codemirror
+        }
+      },
+      mounted() {
+        console.log('the current CodeMirror instance object:', this.codemirror)
+        // you can use this.codemirror to do something...
+      }
   }
 </script>
